e18e127ade4ce7abd99ef1e76e0e8a09525d166a
Fyne Tonye Angala <66461071+Phynnex@users.noreply.github.com>
2025-07-31 00:17:14 +0100

    refactor: modularize components and context

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,34 +1,31 @@
 
 
 "use client";
-import React, { useState } from "react";
-import Navbar from "@/app/components/Navbar";
-import Sidebar from "@/app/components/Sidebar";
-import ClientContext from "@/app/context/ClientContext";
+import React from "react";
+import { Navbar, Sidebar } from "@/components";
+import { ClientProvider } from "@/context/ClientContext";
+import { useToggle } from "@/hooks/useToggle";
 
 import { PropsWithChildren } from "react";
 
 export default function DashboardLayout({ children }: PropsWithChildren<object>) {
-  const [isCollapsed, setIsCollapsed] = useState(false);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const { value: isCollapsed, set: setIsCollapsed } = useToggle(false);
+  const { value: isMobileMenuOpen, set: setIsMobileMenuOpen } = useToggle(false);
   // Replace this with actual logic to get the client name
-  const clientName = "Client Name";
-
   return (
-    <ClientContext.Provider value={{ clientName: 'John Anderson' }}>
-    <div className="flex">
-      <Sidebar
-        isCollapsed={isCollapsed}
-        setIsCollapsed={setIsCollapsed}
-        isMobileMenuOpen={isMobileMenuOpen}
-        setIsMobileMenuOpen={setIsMobileMenuOpen}
-        clientName={clientName}
-      />
-      <div className="flex-1">
-        <Navbar setIsMobileMenuOpen={setIsMobileMenuOpen} clientName={clientName} />
-        <main>{children}</main>
+    <ClientProvider clientName="John Anderson">
+      <div className="flex">
+        <Sidebar
+          isCollapsed={isCollapsed}
+          setIsCollapsed={setIsCollapsed}
+          isMobileMenuOpen={isMobileMenuOpen}
+          setIsMobileMenuOpen={setIsMobileMenuOpen}
+        />
+        <div className="flex-1">
+          <Navbar setIsMobileMenuOpen={setIsMobileMenuOpen} />
+          <main>{children}</main>
+        </div>
       </div>
-    </div>
-    </ClientContext.Provider>
+    </ClientProvider>
   );
 }
--- src/app/context/ClientContext.tsx
+++
@@ -1,8 +0,0 @@
-// app/context/ClientContext.tsx
-"use client";
-import { createContext, useContext } from 'react';
-
-const ClientContext = createContext({ clientName: 'John Anderson' });
-
-export const useClient = () => useContext(ClientContext);
-export default ClientContext;
