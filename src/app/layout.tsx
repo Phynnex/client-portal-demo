e3b68b72f1d19ca4cf21e8dcfa37263e6a709cf3
Fyne Tonye Angala <66461071+Phynnex@users.noreply.github.com>
2025-08-07 01:46:56 +0100

    feat: add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,17 @@ import "@/app/globals.css";
 import { ReactNode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/queryClient";
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
